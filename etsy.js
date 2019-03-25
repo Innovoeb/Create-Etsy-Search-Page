21c82bc88134d9b294ee3deb5b68845d6fc7b710
Beovonni Glover <47301902+Innovoeb@users.noreply.github.com>
2019-03-24 23:41:04 -0700

    etsy ver 2

---
+++ etsy.js
@@ -0,0 +1,43 @@
+
+
+
+
+let template = `
+<div class="products">
+  <img src ="">
+  <p class="title"></p>
+  <p class="maker"></p>
+  <p class="product-price"></p>
+</div>`
+
+let products = document.querySelector(".json");
+
+function fuckYou(a) {
+  if (a === "results") {
+    return
+  }
+};
+
+function myFunction () {
+  items.filter(fuckYou);
+  console.log();
+}
+
+myFunction();
+
+
+
+
+
+
+
+//let jsonstr = items.map(a => {
+  //return `
+  //<div class="products">
+    //<img src ="${a.Images}">
+  //  <p class="title">${a.title}</p>
+  //  <p class="maker">${a.Shop}</p>
+  //  <p class="product-price">${a.price}</p>
+  //</div>
+//  `
+//}).join("")
